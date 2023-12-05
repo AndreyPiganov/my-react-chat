2b9d5b847d73d83719465d7b5356e9ad0e39af1b
AndreyPiganov <andrey.piganov@mail.ru>
2023-12-05 14:48:19 +0300

    add Navigate Bar

--- frontend/src/components/NavigateBar.jsx
+++ frontend/src/components/NavigateBar.jsx
@@ -1,7 +1,7 @@
 import {Navbar, Container} from 'react-bootstrap';
 
 function NavigateBar(){
-    return (<Navbar expand="lg" className="bg-body-tertiary">
+    return (<Navbar expand="lg" className="bg-body-tertiary shadow-sm">
         <Container>
             <Navbar.Brand href='/'>My Chat</Navbar.Brand>
         </Container>
