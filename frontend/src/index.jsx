993d81c5a7ab97d46f687ba809097af74eeac8ce
AndreyPiganov <andrey.piganov@mail.ru>
2023-12-06 19:26:15 +0300

    add state-manager(redux)

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App.jsx';
+import {Provider} from 'react-redux';
+import store from './store/store.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const app = async () => {
   const root = ReactDOM.createRoot(document.getElementById('root'));
   root.render(
+    <Provider store={store}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
+    </Provider>
   );
 };
 
